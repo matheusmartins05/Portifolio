8611a03bf6d659197e507d7c2a77d8cf12fb20fe
matheusmartins05 <matheusmartins2901@gmail.com>
2024-07-06 13:22:55 -0300

    Versao mobile da seção sobre finalizada

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -1,29 +1,33 @@
 
-import Blur from "../components/Blur";
+import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import Wrapper from "../components/Wrapper";
 import minhaFoto from "/minhaFoto2.png"
 
 export default function Sobre() {
   return (
-      <section className="bg-background py-20 md:py-10 h-screen">
-        <Wrapper width="md:w-[60%] w-[90%] mx-auto">
-          <div className="flex justify-between items-center h-96 text-center">
-            <figure>
-              <Blur blur="absolute top-[50%] left-[50%] w-px"></Blur>
-              {/* <img className="rounded-md max-w-72 " src={minhaFoto} alt="" /> */}
-            </figure>
+      <section className="bg-background py-20 sm:py-10 h-screen">
+        <Wrapper width="sm:w-[60%] w-[90%] mx-auto">
+          <div className="flex flex-col sm:flex-row sm:justify-between items-center text-center w-[100%] bg-[#121316] rounded-sm">
 
-            <div className="text-heading flex flex-col justify-end gap-4 sm:text-left text-center w-96 h-[100%] relative">
-              <h2 className="text-4xl">Muito prazer, sou Matheus Santos Martins</h2>
-              <div className="h-px w-[100%] bg-paraghaph my-4"></div>
-              <p className="leading-7 mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut numquam quos cumque aliquam nisi, alias magnam, suscipit optio, fuga provident exercitationem itaque earum velit facilis quaerat maxime corporis commodi iste.</p>
+            <div className="text-heading flex flex-col  gap-2 sm:text-left text-center sm:w-96 w-[90%] relative z-10 mt-10 sm:ml-10 ">
+              <h2 className="sm:text-3xl text-2xl bg-gradient-to-r from-[#449064] to-[#655048] text-transparent bg-clip-text">Muito prazer, sou <span className="text">Matheus Santos Martins</span></h2>
+              <div className="h-px sm:w-[80%] bg-[#655048]"></div>
+              <p className="leading-6 sm:mb-4 text-paraghaph text-sm">Sou um desenvolvedor front end, formado em Análise e Desenvolvimento de Sistemas, em busca da minha primeira oportunidade na área. Além de minhas habilidades em programação, sou entusiasta da área de marketing, o que me proporciona uma visão abrangente e criativa para desenvolver interfaces de usuário atrativas e funcionais. Estou motivado a aplicar meus conhecimentos e aprender continuamente, contribuindo de forma significativa para projetos inovadores.</p>
           
-              <div className="flex justify-center md:justify-start gap-4">
-                <Button style="bg-primary text-heading w-32 p-2 rounded-lg"> Tecnologias </Button>
-                <Button style="bg-primary text-heading w-32 p-2 rounded-lg">  Projetos  </Button>
+              <div className="hidden sm:flex justify-center md:justify-start gap-4 text-sm mb-2 ">
+                <Button style="bg-[#655048] text-heading w-32 p-2 rounded-lg"> <Link to={"/tecnologias"}>Tecnologias</Link> </Button>
+                <Button style="bg-[#655048] text-heading w-32 p-2 rounded-lg"> <Link to={"/projetos"}> Projetos </Link>  </Button>
               </div>
             </div>
+
+            <figure className="self-end flex items-end justify-between w-[100%]">
+              <div className=" flex flex-col gap-6 text-xs mb-2 ml-6 sm:hidden w-[35%]">
+                <Button style="bg-[#655048] text-heading w-[100%] sm:w-32 p-2 rounded-lg"> <Link to={"/tecnologias"}>Tecnologias</Link> </Button>
+                <Button style="bg-[#655048] text-heading w-[100%] sm:w-32 p-2 rounded-lg"> <Link to={"/projetos"}> Projetos </Link>  </Button>
+              </div>
+              <img className="sm:w-[100%] max-w-80 w-[50%] float-end" src={minhaFoto} alt="" />
+            </figure>
           </div>
         </Wrapper>
       </section>
