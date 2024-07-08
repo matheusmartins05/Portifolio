9931090f97a9a3f0478241b54c4a477fbcea8b5a
matheusmartins05 <matheusmartins2901@gmail.com>
2024-07-08 09:57:27 -0300

    Finalizada seção sobre

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -6,27 +6,23 @@ import minhaFoto from "/minhaFoto2.png"
 
 export default function Sobre() {
   return (
-      <section className="bg-background py-20 sm:py-10 h-screen">
-        <Wrapper width="sm:w-[60%] w-[90%] mx-auto">
-          <div className="flex flex-col sm:flex-row sm:justify-between items-center text-center w-[100%] bg-[#121316] rounded-sm">
+      <section className="bg-background  lg:py-10 h-screen sm">
+        <Wrapper width="lg:w-[60%] w-[90%] mx-auto">
+          <div className="flex flex-col w-[100%] bg-[#121316] rounded-sm">
 
-            <div className="text-heading flex flex-col  gap-2 sm:text-left text-center sm:w-96 w-[90%] relative z-10 mt-10 sm:ml-10 ">
-              <h2 className="sm:text-3xl text-2xl bg-gradient-to-r from-[#449064] to-[#655048] text-transparent bg-clip-text">Muito prazer, sou <span className="text">Matheus Santos Martins</span></h2>
-              <div className="h-px sm:w-[80%] bg-[#655048]"></div>
-              <p className="leading-6 sm:mb-4 text-paraghaph text-sm">Sou um desenvolvedor front end, formado em Análise e Desenvolvimento de Sistemas, em busca da minha primeira oportunidade na área. Além de minhas habilidades em programação, sou entusiasta da área de marketing, o que me proporciona uma visão abrangente e criativa para desenvolver interfaces de usuário atrativas e funcionais. Estou motivado a aplicar meus conhecimentos e aprender continuamente, contribuindo de forma significativa para projetos inovadores.</p>
+            <div className="text-heading flex flex-col  gap-2 text-left  w-[90%] sm:w-[80%] relative z-10 mt-10 ml-[5%]">
+              <h2 className="lg:text-3xl text-2xl bg-gradient-to-r from-[#449064] to-[#655048] text-transparent bg-clip-text">Muito prazer, sou <span className="text-nowrap">Matheus Santos Martins</span></h2>
+              <div className="h-px w-[80%] sm:w-3/5 md:w-2/4 bg-[#655048] lg:my-2"></div>
+              <p className="leading-6 lg:mb-4 text-paraghaph text-sm sm:w-[70%] ">Sou um desenvolvedor front end, formado em Análise e Desenvolvimento de Sistemas, em busca da minha primeira oportunidade na área. Além de minhas habilidades em programação, sou entusiasta da área de marketing, o que me proporciona uma visão abrangente e criativa para desenvolver interfaces de usuário atrativas e funcionais. Estou motivado a aplicar meus conhecimentos e aprender continuamente, contribuindo de forma significativa para projetos inovadores.</p>
           
-              <div className="hidden sm:flex justify-center md:justify-start gap-4 text-sm mb-2 ">
-                <Button style="bg-[#655048] text-heading w-32 p-2 rounded-lg"> <Link to={"/tecnologias"}>Tecnologias</Link> </Button>
-                <Button style="bg-[#655048] text-heading w-32 p-2 rounded-lg"> <Link to={"/projetos"}> Projetos </Link>  </Button>
-              </div>
             </div>
 
-            <figure className="self-end flex items-end justify-between w-[100%]">
-              <div className=" flex flex-col gap-6 text-xs mb-2 ml-6 sm:hidden w-[35%]">
-                <Button style="bg-[#655048] text-heading w-[100%] sm:w-32 p-2 rounded-lg"> <Link to={"/tecnologias"}>Tecnologias</Link> </Button>
-                <Button style="bg-[#655048] text-heading w-[100%] sm:w-32 p-2 rounded-lg"> <Link to={"/projetos"}> Projetos </Link>  </Button>
+            <figure className="self-end flex items-end justify-between w-[100%]  sm:h-52 lg:h-36">
+              <div className=" flex flex-col md:flex-row md:items-center md:h-[100%] gap-6 text-sm sm:text-base mb-2 lg:mb-4 ml-[5%] w-[100%]">
+                <Button style="bg-[#655048] text-heading w-[80%] md:h-[20%] lg:h-[28%] md:w-[40%] lg:w-32 p-2 rounded-lg"> <Link to={"/tecnologias"}>Tecnologias</Link> </Button>
+                <Button style="bg-[#655048] text-heading w-[80%] md:h-[20%] md:w-[40%] lg:h-[28%] lg:w-32 p-2 rounded-lg"> <Link to={"/projetos"}> Projetos </Link>  </Button>
               </div>
-              <img className="sm:w-[100%] max-w-80 w-[50%] float-end" src={minhaFoto} alt="" />
+              <img className=" max-w-80 w-[50%] float-end" src={minhaFoto} alt="" />
             </figure>
           </div>
         </Wrapper>
