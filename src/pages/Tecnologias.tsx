e421c291543835455bc599dae1395bde0f63c073
matheusmartins05 <matheusmartins2901@gmail.com>
2024-07-09 19:32:10 -0300

    Finalizada seção tecnologias

--- src/pages/Tecnologias.tsx
+++ src/pages/Tecnologias.tsx
@@ -5,11 +5,11 @@ import  tecnologias  from "../components/techs.json";
 export default function Tecnologias(){
     return(
         <Wrapper width="md:w-[80%] w-[90%] mx-auto">
-            <section className="md:py-20 py-10  text-center font-poppins leading-5">
-                <h1 className="md:text-4xl text-2xl text-primary">Minhas habilidades</h1>
+            <section className="md:py-12 py-10  text-center font-poppins leading-5">
+                <h1 className="md:text-4xl text-2xl bg-gradient-to-r from-[#449064] to-[#655048] text-transparent bg-clip-text">Minhas habilidades</h1>
                 <p className="text-paraghaph mt-2">Tecnologias que tenho trabalhado recentemente.</p>
 
-                <div className="flex flex-wrap gap-4 justify-between  flex-1  mx-auto py-10 w-[90%]">
+                <div className="flex flex-wrap gap-8 justify-between  flex-1  mx-auto py-10 w-[90%]">
                     {tecnologias.map((element) => <CardTecnologia imgTech={element.imagem} nomeTech={element.nome} key={element.id}/>)}
                 </div>
             </section>
