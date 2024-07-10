5650639bb8dd20d6446e121826156765aca0e094
matheusmartins05 <matheusmartins2901@gmail.com>
2024-07-10 10:30:54 -0300

    criado versão estática da seção projetos

---
+++ src/components/CardProjeto.tsx
@@ -0,0 +1,29 @@
+import exemploProjeto from "/exemploProjeto.png";
+import typescript from "/typescript.svg";
+import react from "/react.svg";
+import tailwindcss from "/tailwindcss.svg";
+
+export default function CardProjeto() {
+  return (
+    <div className="bg-[#121316] h-[350px] w-96 rounded-lg">
+      <img
+        className="w-[90%] mx-auto mt-4 rounded-t-lg h-[50%]"
+        src={exemploProjeto}
+        alt=""
+      />
+      <div className="flex flex-col w-[85%] mx-auto my-4">
+        <div className="flex gap-4">
+          <img className="w-4" src={typescript} alt="" />
+          <img className="w-4" src={react} alt="" />
+          <img className="w-4" src={tailwindcss} alt="" />
+        </div>
+        <h4 className="text-[#655048] mt-2">Nome do Projeto</h4>
+        <p className="text-xs text-paraghaph">
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+          ipsam nobis quibusdam nisi dicta numquam quia illum.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Projetos.tsx
+++ src/pages/Projetos.tsx
@@ -1,11 +1,17 @@
+import CardProjeto from "../components/CardProjeto";
 import Wrapper from "../components/Wrapper";
 
 export default function Projetos() {
   return (
     <>
     <Wrapper width="w-[80%] ">
-      <div>
-          <h1 className="text-white"> PROJETOS!</h1>
+      <h3 className="text-3xl text-heading text-center mt-8">Projetos</h3>
+      <div className="flex gap-4 py-8">
+
+          <CardProjeto/>
+          <CardProjeto/>
+          <CardProjeto/>
+          <CardProjeto/>
       </div>
     </Wrapper>
     </>
